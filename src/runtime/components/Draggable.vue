8c03b6bcb607ff63ce749ee552a314922ae8bb41
Tun Cham Roeun <chamroeuntun25@outlook.com>
2026-04-28 10:10:30 +0700

    Revert "fix fast component"
    This reverts commit 27e7688248fdd77fae6997d72179dfb1945ae49d.

--- src/runtime/components/Draggable.vue
+++ src/runtime/components/Draggable.vue
@@ -147,15 +147,6 @@ const realList = computed<T[] | null>(() => {
   return props.list ? props.list : props.modelValue
 })
 
-function buildComponentStructure(): ComponentStructure {
-  return computeComponentStructure({
-    $slots: slots,
-    tag: props.tag,
-    realList: realList.value,
-    getKey: getKey.value,
-  })
-}
-
 const getKey = computed<(element: T) => string | number>(() => {
   const { itemKey } = props
   if (typeof itemKey === 'function') {
@@ -398,15 +389,6 @@ onMounted(() => {
   }
 
   const $el = instance.proxy!.$el as HTMLElement
-  if (!componentStructure) {
-    try {
-      componentStructure = buildComponentStructure()
-    }
-    catch {
-      // RenderContent handles reporting details for structure build failures.
-      return
-    }
-  }
   componentStructure.updated()
 
   const callBackBuilder: CallBackBuilder = {
@@ -452,7 +434,12 @@ onBeforeUnmount(() => {
 const RenderContent = (): VNode => {
   try {
     error.value = false
-    componentStructure = buildComponentStructure()
+    componentStructure = computeComponentStructure({
+      $slots: slots,
+      tag: props.tag,
+      realList: realList.value,
+      getKey: getKey.value,
+    })
     const attributes = getComponentAttributes({
       $attrs: attrs,
       componentData: props.componentData ?? undefined,
